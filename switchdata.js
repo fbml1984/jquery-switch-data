80954ba77f7e4049fbffd0517751693740701fa9
Fábio Lazaro <fsbrancaglioni@gmail.com>
2018-06-27 19:16:20 -0300

    Commit inicial

---
+++ switchdata.js
@@ -0,0 +1,274 @@
+(function ($) {
+    $.fn.switchData = function (options) {
+        var settings = $.extend({
+            receiveFromExternal: false,
+            receiveFrom: null,
+            observeWhenChange: null,
+            search: {
+                url: null,
+                resultPair: {
+                    key: 'key',
+                    value: 'value'
+                },
+                placeholder: null
+            }
+        }, options);
+        //...
+        return this.each(function (i, $dataFrom) {
+
+            var $selectLeft = $($dataFrom),
+                $parent = $selectLeft.closest('div[class*="jumbotron"]'),
+                $selectRight = $(".switch-left", $parent),
+                $optionsSelectRight = $('option', $selectRight),
+                $btnMoveToLeft = $('[class*="left-all"]', $parent),
+                $btnMoveToRight = $('[class*="right-all"]', $parent),
+                $selectToObserve = $(settings.observeWhenChange, $parent);
+
+            if (settings.receiveFromExternal && settings.receiveFrom !== null && settings.observeWhenChange !== null && $selectToObserve.length) {
+                $selectToObserve.on('change', function () {
+                    $.when(settings.receiveFrom($(this).val())).done(function (response) {
+                        if (response.length || response !== null) {
+                            var items = [];
+                            $.each(response, function (key, value) {
+                                items.push(_createLIFromKeyAndValue(value, key, 'destiny'));
+                            });
+                            $.when($leftUL.html(items)).done(function () {
+                                _checkDuplicatedValues();
+                            });
+                        }
+                    }).fail(function (error) {
+                        $leftUL.html("");
+                        console.log(error);
+                    });
+                });
+            }
+
+            if (settings.search && settings.search.url !== undefined && settings.search.url != null) {
+                var $inputSearch = $('<input/>')
+                    .attr('type', 'text')
+                    .attr('name', 'q')
+                    .attr('placeholder', options.search.placeholder)
+                    .addClass('form-control');
+
+                var $clear = $('<button/>')
+                    .attr('type', 'button');
+
+                $clear.on('click', function () {
+                    $inputSearch.val("");
+                    $leftUL.html("");
+                });
+
+                $inputSearch.insertBefore($selectLeft);
+                $clear.insertBefore($selectLeft);
+
+                $('[name="q"]', $parent).on('keyup', function () {
+                    _fireSearch(this.value);
+                });
+            }
+
+            var $leftUL = $('<ul/>').attr('id', $selectLeft.attr('name'));
+            $selectLeft.parent().append($leftUL);
+
+            var $rightUL = $('<ul/>').attr('id', $selectRight.attr('name'));
+            $selectRight.parent().append($rightUL);
+
+            if (typeof $optionsSelectRight !== 'undefined' &&
+                $optionsSelectRight !== undefined &&
+                $optionsSelectRight !== null &&
+                $optionsSelectRight.length > 0
+            ) {
+                var rightListLI = [];
+                $.each($optionsSelectRight, function (i, option) {
+                    $(option).attr('selected', true);
+                    var $rightLI = _createLIFromOption(option, 'origin');
+                    rightListLI.push($rightLI);
+                });
+                $($rightUL).html(rightListLI);
+            }
+
+            $btnMoveToLeft.on('click', function () {
+                var countLeftLI = $('li', $leftUL).length;
+                $('li', $rightUL).each(function (i, rightLI) {
+                    var rightID = $(rightLI).attr('id');
+                    var _append = true;
+                    if (countLeftLI) {
+                        $('li', $leftUL).each(function (i, leftLI) {
+                            var leftID = $(leftLI).attr('id');
+                            if (rightID === leftID) {
+                                return _append = false;
+                            }
+                        });
+                    }
+                    if (_append) {
+                        _createLIFromLI(rightLI, 'destiny').appendTo($leftUL);
+                        $('option[value="' + rightID + '"]:selected', $selectRight).remove();
+                        $(rightLI).remove();
+                    }
+                    _sortList($leftUL[0]);
+                });
+            });
+
+            $btnMoveToRight.on('click', function () {
+                var countRightLI = $('li', $rightUL).length;
+                $.each($('li', $leftUL), function (i, leftLI) {
+                    var leftID = $(leftLI).attr('id');
+                    var _append = true;
+                    if (countRightLI) {
+                        $('li', $rightUL).each(function (i, rightLI) {
+                            var rightID = $(rightLI).attr('id');
+                            if (leftID === rightID) {
+                                return _append = false;
+                            }
+                        });
+                    }
+                    if (_append) {
+                        _createLIFromLI(leftLI, 'origin').appendTo($rightUL);
+                        _createOptionFromLI(leftLI).appendTo($selectRight);
+                        $(leftLI).remove();
+                    }
+                    _sortList($rightUL[0]);
+                });
+            });
+
+            function _checkDuplicatedValues() {
+                var countLeftLI = $('li', $leftUL).length;
+                $('li', $rightUL).each(function (i, rightLI) {
+                    var rightID = $(rightLI).attr('id');
+                    if (countLeftLI) {
+                        $('li', $leftUL).each(function (i, leftLI) {
+                            var leftID = $(leftLI).attr('id');
+                            if (rightID === leftID) {
+                                $(leftLI).addClass('repeated');
+                            }
+                        });
+                    }
+                });
+            }
+
+            function _move(item, direction) {
+                let id = $(item).attr('id');
+                let text = $(item).text();
+                if (direction === 'destiny') {
+                    var exists = $('option[value="' + id + '"]', $selectRight).length;
+                    if (exists) {
+                        return false;
+                    }
+                    $rightUL.append(_createLIFromKeyAndValue(id, text, 'origin'));
+                    $selectRight.append(_createOptionFromKeyAndValue(id, text, true));
+                    $(item, $leftUL).remove();
+                } else if (direction === 'origin') {
+                    var exists = $('li[id="' + id + '"]', $leftUL).length;
+                    if (exists) {
+                        return false;
+                    }
+                    $leftUL.append(_createLIFromKeyAndValue(id, text, 'destiny'));
+                    $('option[value="' + id + '"]', $selectRight).remove();
+                    $(item, $rightUL).remove();
+                }
+                _sortList($leftUL[0]);
+                _sortList($rightUL[0]);
+            }
+
+            function _fireSearch(q) {
+                if (q.length < 3) {
+                    $leftUL.html("");
+                    return false;
+                }
+                // ...
+                $.ajax({
+                    method: "GET",
+                    url: options.search.url.replace("%QUERY", q)
+                }).done(function (response) {
+                    if (response.length) {
+                        var items = [];
+                        for (var i in response) {
+                            items.push(_createLIFromKeyAndValue(response[i][options.search.resultPair.key], response[i][options.search.resultPair.value], 'destiny'));
+                        }
+                        $.when($leftUL.html(items)).done(function () {
+                            _checkDuplicatedValues();
+                        });
+                    }
+                }).fail(function () {
+                    $leftUL.html("");
+                    console.log("Erro ao pesquisar os usuários cujo nome contenha [termo=" + q + "]");
+                });
+            }
+
+            function _createLIFromKeyAndValue(id, text, direction) {
+                return $('<li/>')
+                    .attr('id', id)
+                    .html(text)
+                    .on('click', function () {
+                        _move(this, direction);
+                    });
+            }
+
+            function _createLIFromLI(li, direction) {
+                return $('<li/>')
+                    .attr('id', $(li).attr('id'))
+                    .html($(li).text())
+                    .on('click', function () {
+                        _move(this, direction);
+                    });
+            }
+
+            function _createLIFromOption(option, direction) {
+                return $('<li/>')
+                    .attr('id', $(option).val())
+                    .html($(option).text())
+                    .on('click', function () {
+                        _move(this, direction);
+                    });
+            }
+
+            function _createOptionFromKeyAndValue(id, text, selected) {
+                return $('<option/>')
+                    .attr('value', id)
+                    .attr('selected', selected)
+                    .html(text);
+            }
+
+            function _createOptionFromLI(li) {
+                return $('<option/>')
+                    .attr('value', $(li).attr('id'))
+                    .attr('selected', true)
+                    .html($(li).text());
+            }
+
+            function _sortList(list) {
+                if (list === undefined) {
+                    return;
+                }
+                var i, switching, b, shouldSwitch;
+                switching = true;
+                /*Make a loop that will continue until
+                no switching has been done:*/
+                while (switching) {
+                    //start by saying: no switching is done:
+                    switching = false;
+                    b = list.getElementsByTagName("LI");
+                    //Loop through all list-items:
+                    for (i = 0; i < (b.length - 1); i++) {
+                        //start by saying there should be no switching:
+                        shouldSwitch = false;
+                        /*check if the next item should
+                        switch place with the current item:*/
+                        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                            /*if next item is alphabetically
+                            lower than current item, mark as a switch
+                            and break the loop:*/
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                    if (shouldSwitch) {
+                        /*If a switch has been marked, make the switch
+                        and mark the switch as done:*/
+                        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+                        switching = true;
+                    }
+                }
+            }
+        });
+    };
+}(jQuery));
